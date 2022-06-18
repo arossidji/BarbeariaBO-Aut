aef78f759f3c5d138c0156530ee23a690135c3d4
Augusto Rossi <augusto.eletro@hotmail.com>
2022-06-18 16:27:58 -0300

    Projeto inicial adicionado

---
+++ src/main/java/com/br/barbeariabo/BarbeariaBoApplication.java
@@ -0,0 +1,13 @@
+package com.br.barbeariabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaBoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaBoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/barbeariabo/BarbeariaBoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.barbeariabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaBoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
